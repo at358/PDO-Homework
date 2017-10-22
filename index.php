d05984edf6bb8d9f6c6220d71b04517294d200cf
at358 <at358@njit.edu>
2017-10-22 19:47:06 -0400

    PDO Statement Code

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
+<html>
+    <head>
+        <title>PDO Demo</title>
+    </head>
+    <body>
+        <table = border="1">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+            </tr>
+        </thead>
+        <tbody>
+        <table>
 		<?php
 			$username = 'at358';
 			$password = 'Krishna15';
 			$hostname = 'sql1.njit.edu';
 			$dsn = "mysql:host=$hostname;dbname=$username";
 
-		try {
-		    $conn = new PDO($dsn, $username, $password);
-    		echo "Connected successfully<br>";
-    		$conn = null;
-    		$stmt->bindValue(':id', $id); 
-    		$stmt = $conn->prepare("SELECT id, email FROM accounts LIMIT 5"); 
-    		$stmt->execute();
-    		$result = $stmt->fetchAll()); 
-    		$stmt->closerCursor();
-    		$count = $stmt->rowCount();
-			echo "Results: $count<br>";
-		catch(PDOException $e) {
-    	echo "Error: " . $e->getMessage();
-							   }
+		mysql_connect($dsn, $username, $password) or die("Could not connect: " . mysql_error()); 
 		
-    	foreach ($accounts as $account) { ?>
-		<table border="1">
-    		<th>
-        		<td>ID</td>
-        		<td>Email</td>
-    		</th>
-    		<tr>
-        		<td><?php echo $account['id']; ?></td>
-        		<td><?php echo $account['email']; ?></td>
-    		</tr>
-		</table>
-    			}
-			}
-		
+		$result = mysql_query("SELECT id, email FROM accounts LIMIT 5");
+
+		foreach($result['id','email'] as $rows):?>
+        <tr class="item_row">
+            <td> <?php echo $rows['id']; ?></td>
+            <td> <?php echo $rows['email']; ?></td>
 		?>
+        </tr>
+        </tbody>
+        </table>
+    </body>
+</html>
+		
+	
    
