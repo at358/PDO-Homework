f628e755af1aa5c7f7f47644062d8ed70f9e3d8f
at358 <at358@njit.edu>
2017-10-22 19:25:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
-<?php
+		<?php
+			$username = 'at358';
+			$password = 'Krishna15';
+			$hostname = 'sql1.njit.edu';
+			$dsn = "mysql:host=$hostname;dbname=$username";
 
-echo "<h1>PDO demo!</h1>";
-$username = '<ucid here>';
-$password = '<password here>';
-$hostname = 'sql.njit.edu';
-
-$dsn = "mysql:host=$hostname;dbname=$username";
-
-try {
-    $conn = new PDO($dsn, $username, $password);
-    echo "Connected successfully<br>";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-$conn = null;
-
-?>
+		try {
+		    $conn = new PDO($dsn, $username, $password);
+    		echo "Connected successfully<br>";
+    		$conn = null;
+    		$stmt->bindValue(':id', $id); 
+    		$stmt = $conn->prepare("SELECT id, email FROM accounts LIMIT 5"); 
+    		$stmt->execute();
+    		$result = $stmt->fetchAll()); 
+    		$stmt->closerCursor();
+    		$count = $stmt->rowCount();
+			echo "Results: $count<br>";
+		catch(PDOException $e) {
+    	echo "Error: " . $e->getMessage();
+							   }
+		
+    	foreach ($accounts as $account) { ?>
+		<table border="1">
+    		<th>
+        		<td>ID</td>
+        		<td>Email</td>
+    		</th>
+    		<tr>
+        		<td><?php echo $account['id']; ?></td>
+        		<td><?php echo $account['email']; ?></td>
+    		</tr>
+		</table>
+    			}
+			}
+		
+		?>
+   
