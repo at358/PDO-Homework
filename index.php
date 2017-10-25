9992a08735603a52b1644479c34547c5890e9dbf
at358 <at358@njit.edu>
2017-10-25 08:22:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
         <title>PDO Demo</title>
     </head>
     <body>
-        <table = border="1">
-        <thead>
-            <tr>
-                <td>Id</td>
-                <td>Name</td>
-            </tr>
-        </thead>
-        <tbody>
-        <table>
-	<?php
+        <?php
 			$username = 'at358';
 			$password = 'Krishna15';
 			$hostname = 'sql1.njit.edu';
@@ -27,8 +18,8 @@
 	$statement->execute();
 	$accounts = $statement->fetchAll();
 	$statement->closeCursor();
-	
-	foreach ($accounts as account):
+	<table = border="1">
+        foreach ($accounts as account):
 	<tr>
     	<td><?php echo $accounts['ID']; ?></td>
     	<td><?php echo $accounts['Email']; ?></td>
